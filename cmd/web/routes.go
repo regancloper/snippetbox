52f0cf63afaf31f0e61b867bd07fe0e7f407cb48
Regan Loper <regan.loper@shipt.com>
2023-08-04 10:38:40 -0500

    chapter 6.5

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -1,6 +1,10 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+
+	"github.com/justinas/alice"
+)
 
 // The routes() method used to return a servemux containing our application routes.
 // *Update* the signature for the routes() method so that it returns a
@@ -24,10 +28,10 @@ func (app *application) routes() http.Handler {
 	mux.HandleFunc("/snippet/view", app.snippetView)
 	mux.HandleFunc("/snippet/create", app.snippetCreate)
 
-	// Pass the servemux as the 'next' parameter to the secureHeaders middleware.
-	// Because secureHeaders is just a function, and the function returns a
-	// http.Handler we don't need to do anything else.
-	// Wrap the existing chain with the logRequest middleware.
-	// Wrap the existing chain with the recoverPanic middleware.
-	return app.recoverPanic(app.logRequest(secureHeaders(mux)))
+	// Create a middleware chain containing our 'standard' middleware
+	// which will be used for every request our application receives.
+	standard := alice.New(app.recoverPanic, app.logRequest, secureHeaders)
+
+	// Return the 'standard' middleware chain followed by the servemux.
+	return standard.Then(mux)
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module snippetbox.reganloper.com
 
 go 1.20
 
-require github.com/go-sql-driver/mysql v1.7.1
+require (
+	github.com/go-sql-driver/mysql v1.7.1
+	github.com/justinas/alice v1.2.0
+)
