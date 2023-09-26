7dea5d351d62c134fb93cf4fbbbed41b448f06b8
Regan Loper <regan.loper@shipt.com>
2023-09-26 10:30:29 -0500

    chapter 11.4

--- internal/validator/validator.go
+++ internal/validator/validator.go
@@ -15,13 +15,23 @@ var EmailRX = regexp.MustCompile("^[a-zA-Z0-9.!#$%&'*+\\/=?^_`{|}~-]+@[a-zA-Z0-9
 
 // Define a new Validator type which contains a map of validation errors for our
 // form fields.
+// Add a new NonFieldErrors []string field to the struct, which we will use to
+// hold any validation errors which are not related to a specific form field.
 type Validator struct {
-	FieldErrors map[string]string
+	NonFieldErrors []string
+	FieldErrors    map[string]string
 }
 
-// Valid() returns true if the FieldErrors map doesn't contain any entries.
+// Valid() returns true if the FieldErrors map doesn't contain any entries AND
+// the NonFieldErrors slice is empty.
 func (v *Validator) Valid() bool {
-	return len(v.FieldErrors) == 0
+	return len(v.FieldErrors) == 0 && len(v.NonFieldErrors) == 0
+}
+
+// Create an AddNonFieldError() helper for adding error messages to the new
+// NonFieldErrors slice.
+func (v *Validator) AddNonFieldError(message string) {
+	v.NonFieldErrors = append(v.NonFieldErrors, message)
 }
 
 // AddFieldError() adds an error message to the FieldErrors map (so long as no
